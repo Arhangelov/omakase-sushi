2b00d8b9c6b2085dca65786fd38c03c821682067
Arhangelov <arhangelov93@gmail.com>
2023-03-27 23:36:19 +0300

    added index.js and routes.js

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const config = require("./config/main");
+const express = require("express");
+const db = require("./config/mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const router = require("./routes");
+
+const app = express();
+
+app.use(cors({ credentials: true }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
+
+db().then(() => {
+    console.log("Connected to database");
+    app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}`))
+});
