67a2d95e4975fedac97c6e7c0f82b688d11f9203
Arhangelov <arhangelov93@gmail.com>
2023-03-27 23:58:33 +0300

    created database model for users

---
+++ server/models/User.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    email : {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minlength: 9,
+    },
+
+    address: {
+        type: String,
+        required: true,
+    },
+
+    cart: [],
+    purchaseHistory: []
+
+});
+
+userSchema.pre('save', function(next) {
+    bcrypt.genSalt(10)
+    .then(salt => bcrypt.hash(this.password, salt))
+    .then(hash => {
+        this.password = hash;
+        next();
+    });
+});
+
+module.exports = mongoose.model('User', userSchema);
