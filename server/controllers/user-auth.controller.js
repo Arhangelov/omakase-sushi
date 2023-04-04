dbca731dd107628d75d768e9fe6edd1629f5d509
Arhangelov <arhangelov93@gmail.com>
2023-04-04 19:48:26 +0300

    added login rout in user-auth controller

--- server/controllers/user-auth.controller.js
+++ server/controllers/user-auth.controller.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { COOKIE_NAME } = require("../config/main");
 const { register } = require("../services/user-auth.service");
-const { registerUserReqValidation } = require("../middleware/user-auth.validation");
+const { registerUserReqValidation, loginUserReqValidation } = require("../middleware/user-auth.validation");
 const { validationResult } = require("express-validator");
 
 router.post("/register", registerUserReqValidation, (req, res) => {
@@ -12,6 +12,7 @@ router.post("/register", registerUserReqValidation, (req, res) => {
 
     register(req.body)
     .then(({ newUserDTO, token }) => {
+        console.log(req.body);
         res.status(201)
         .cookie(COOKIE_NAME, token, { httpOnly: true })
         .json({ newUserDTO, token })
@@ -22,5 +23,26 @@ router.post("/register", registerUserReqValidation, (req, res) => {
         });
     });
 });
+
+router.post('/login',
+    loginUserReqValidation,
+    (req, res) => {
+        const errors = validationResult(req); // Saving any occurred errors.
+        // If there're errors mark as Bad Request 400 and return error list.
+        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() }); 
+        else validationResult(req).throw(); // Else clear validation results and continue with the response.
+
+        login(req.body.email)
+        .then(({userDTO, token}) => {
+            res.status(200)
+            .cookie(COOKIE_NAME, token, { httpOnly: true })
+            .json({userDTO, token})
+        }).catch((err) => {
+            return res.status(400).send({
+                message: `${err.message}`,
+                type: "ERROR",
+            });
+        });
+    });
 
 module.exports = router;
