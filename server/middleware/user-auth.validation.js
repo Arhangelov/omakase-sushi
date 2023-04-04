9e7adb184ff2eb07673155f0cb1d2bc2d726f3d0
Arhangelov <arhangelov93@gmail.com>
2023-04-04 19:47:30 +0300

    added login validation

--- server/middleware/user-auth.validation.js
+++ server/middleware/user-auth.validation.js
@@ -43,9 +43,41 @@ const registerUserReqValidation = [
         }
         // Indicates the success of this synchronous custom validator
         return true;
-    })
+    }),
+
+    body('address')
+    .exists({ checkFalsy: true })
+    .notEmpty().withMessage("Address field is required.")
+    .isLength({ min: 12, max: 100 }).withMessage("Address must be at least 12 and less than 100 characters.")
+    .optional({ checkFalsy: true })
 ]
 
+const loginUserReqValidation = [
+    body('email')
+    .exists({ checkFalsy: true })
+    .withMessage("Email is required.")
+    .isEmail()
+    .custom(async email => {
+        // Checking if the input email already exists.
+        const existingUser = await User.findOne({ email });
+        // Rejecting the request if there is no such user found.
+        if (!existingUser) return Promise.reject(`No such user.`);
+    }),
+
+    body('password')
+    .exists({ checkFalsy: true })
+    .withMessage("Password is required.")
+    .custom(async ( password, { req }) => {
+        const userEmail = req.body.email;
+        const user = await User.findOne({ userEmail });
+        // Checking if the compared passwords are equal. 
+        const passwordCompare = bcrypt.compare(password, user.password);
+        // Rejecting the request if the passwords are not equal.
+        if (!passwordCompare) return Promise.reject("Invalid password.");
+    }),
+];
+
 module.exports = {
-    registerUserReqValidation
+    registerUserReqValidation,
+    loginUserReqValidation
 }
