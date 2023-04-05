27d481ee3f61c2027d3314e8c866beb2c6e9c07f
Arhangelov <arhangelov93@gmail.com>
2023-04-05 03:22:55 +0300

    added rout for create sushi controller

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userAuthController = require("./controllers/user-auth.controller");
+const createSushiController = require("./controllers/sushi-create.controller");
 
 router.use("/auth", userAuthController);
+router.use("/createSushi", createSushiController);
 
 module.exports = router;
