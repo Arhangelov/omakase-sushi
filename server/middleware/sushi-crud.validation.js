a9170b6f0f46e9e74a8fc5dfb085e3c078ec7c34
Arhangelov <arhangelov93@gmail.com>
2023-04-05 03:57:56 +0300

    created sushi add validation

---
+++ server/middleware/sushi-crud.validation.js
@@ -0,0 +1,64 @@
+const { body } = require("express-validator");
+const Sushi = require("../models/Sushi");
+
+const sushiAddReqValidation = [
+    body("title")
+        .exists({ checkFalsy: true })
+        .withMessage("Title is required.")
+        .custom(async title => {
+            // Checking if the input title already exists.
+            const existingSushiTitle = await Sushi.findOne({ title });
+            // Rejecting the request if the sushi title already exists.
+            if (existingSushiTitle) return Promise.reject(`Sushi "${title}" already exists.`);
+        })
+        .isLength({ min: 13, max: 60 })
+        .withMessage("Title should be between 13 and 60 characters long.")
+        .optional({ checkFalsy: true }),
+
+    body("imageUrl")
+        .exists({ checkFalsy: true })
+        .withMessage("Image URL is required.")
+        .custom(async imageUrl => {
+            // Checking if the input imageUrl already exists.
+            const existingSushiUrl = await Sushi.findOne({ imageUrl });
+            // Rejecting the request if the sushi URL already exists.
+            if (existingSushiUrl) return Promise.reject("Sushi URL already exists.");
+        })
+        .optional({ checkFalsy: true }),
+
+    body("description")
+        .exists({ checkFalsy: true })
+        .withMessage("Description is required.")
+        .custom(async description => {
+            // Checking if the input title already exists.
+            const existingSushiDescription = await Sushi.findOne({ description });
+            // Rejecting the request if the sushi title already exists.
+            if (existingSushiDescription) return Promise.reject(`Sushi description already exists.`);
+        })
+        .isLength({ min: 13, max: 160 })
+        .withMessage("Description should be between 13 and 160 characters long."),
+
+    body("type")
+        .exists({ checkFalsy: true })
+        .withMessage("Type is required.")
+        .isLength({ min: 3, max: 12 })
+        .withMessage("Type should be between 3 and 12 characters long."),
+
+    body("portion")
+        .exists({ checkFalsy: true })
+        .withMessage("Portion is required.")
+        .isLength({ min: 6, max: 12 })
+        .withMessage("Portion should be between 7 and 12 characters."),
+    
+    body("price")
+        .exists({ checkFalsy: true })
+        .withMessage("Price is required.")
+        .isNumeric()
+        .withMessage("Price should be number.")
+        .isLength({ min: 4, max: 6 })
+        .withMessage("Price should be between 4 and 6 characters.")
+]
+
+module.exports = {
+    sushiAddReqValidation,
+}
--- server/middleware/user-auth.validation.js
+++ server/middleware/user-auth.validation.js
@@ -4,79 +4,79 @@ const bcrypt = require("bcrypt");
 
 const registerUserReqValidation = [
     body("email")
-    .exists({ checkFalsy: true })
-    .withMessage("Email is required.")
-    .isEmail()
-    .trim()
-    .normalizeEmail({ gmail_remove_dots: false })
-    .withMessage("Email address is invalid.")
-    .custom(async email => {
-        // Checking if the input email already exists.
-        const existingEmail = await User.findOne({ email: email });
-        // Rejecting the request if there is a duplicate.
-        if (existingEmail) return Promise.reject(`Email ${email} already exists.`);
-    }),
+        .exists({ checkFalsy: true })
+        .withMessage("Email is required.")
+        .isEmail()
+        .trim()
+        .normalizeEmail({ gmail_remove_dots: false })
+        .withMessage("Email address is invalid.")
+        .custom(async email => {
+            // Checking if the input email already exists.
+            const existingEmail = await User.findOne({ email: email });
+            // Rejecting the request if there is a duplicate.
+            if (existingEmail) return Promise.reject(`Email ${email} already exists.`);
+        }),
 
     body("username")
-    .exists({ checkFalsy: true })
-    .withMessage("Username is required.")
-    .custom(async username => {
-        // Checking if the input username already exists.
-        const existingUser = await User.findOne({ username });
-        // Rejecting the request if there is a duplicate.
-        if (existingUser) return Promise.reject(`Username ${username} is already taken.`);
-    })
-    .isAlphanumeric()
-    .withMessage("Username can't include any special symbols.")
-    .isLength({ min: 5, max: 28 })
-    .withMessage("Username should be between 5 and 28 characters long."),
+        .exists({ checkFalsy: true })
+        .withMessage("Username is required.")
+        .custom(async username => {
+            // Checking if the input username already exists.
+            const existingUser = await User.findOne({ username });
+            // Rejecting the request if there is a duplicate.
+            if (existingUser) return Promise.reject(`Username ${username} is already taken.`);
+        })
+        .isAlphanumeric()
+        .withMessage("Username can't include any special symbols.")
+        .isLength({ min: 5, max: 28 })
+        .withMessage("Username should be between 5 and 28 characters long."),
 
     body("password")
-    .exists({ checkFalsy: true })
-    .withMessage("Password is required.")
-    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,32}$/)
-    .withMessage("Password must be between 8 and 32 characters long, must contain at least 1 uppercase and 1 lowercase letter, 1 digit and 1 special character."),
+        .exists({ checkFalsy: true })
+        .withMessage("Password is required.")
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,32}$/)
+        .withMessage("Password must be between 8 and 32 characters long, must contain at least 1 uppercase and 1 lowercase letter, 1 digit and 1 special character."),
 
-    body('confirmPassword').custom((value, { req }) => {
-        if (value !== req.body.password) {
-            throw new Error("Password does not match.");
-        }
-        // Indicates the success of this synchronous custom validator
-        return true;
-    }),
+        body('confirmPassword').custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error("Password does not match.");
+            }
+            // Indicates the success of this synchronous custom validator
+            return true;
+        }),
 
     body("address")
-    .exists({ checkFalsy: true })
-    .notEmpty().withMessage("Address field is required.")
-    .isLength({ min: 12, max: 100 }).withMessage("Address must be at least 12 and less than 100 characters.")
-    .optional({ checkFalsy: true })
+        .exists({ checkFalsy: true })
+        .notEmpty().withMessage("Address field is required.")
+        .isLength({ min: 12, max: 100 }).withMessage("Address must be at least 12 and less than 100 characters.")
+        .optional({ checkFalsy: true })
 ]
 
 const loginUserReqValidation = [
     body("email")
-    .exists({ checkFalsy: true })
-    .withMessage("Email is required.")
-    .isEmail()
-    .trim()
-    .normalizeEmail({ gmail_remove_dots: false })
-    .custom(async email => {
-        // Checking if the input email already exists.
-        const existingUser = await User.findOne({ email });
-        // Rejecting the request if there is no such user found.
-        if (!existingUser) return Promise.reject(`No such user.`);
-    }),
+        .exists({ checkFalsy: true })
+        .withMessage("Email is required.")
+        .isEmail()
+        .trim()
+        .normalizeEmail({ gmail_remove_dots: false })
+        .custom(async email => {
+            // Checking if the input email already exists.
+            const existingUser = await User.findOne({ email });
+            // Rejecting the request if there is no such user found.
+            if (!existingUser) return Promise.reject(`No such user.`);
+        }),
 
     body("password")
-    .exists({ checkFalsy: true })
-    .withMessage("Password is required.")
-    .custom(async ( password, { req }) => {
-        const userEmail = req.body.email;
-        const user = await User.findOne({ email: userEmail });
-        // Checking if the compared passwords are equal.
-        const passwordCompare = bcrypt.compareSync(password, user.password);
-        // Rejecting the request if the passwords are not equal.
-        if (!passwordCompare) return Promise.reject("Invalid password.");
-    })
+        .exists({ checkFalsy: true })
+        .withMessage("Password is required.")
+        .custom(async ( password, { req }) => {
+            const userEmail = req.body.email;
+            const user = await User.findOne({ email: userEmail });
+            // Checking if the compared passwords are equal.
+            const passwordCompare = bcrypt.compareSync(password, user.password);
+            // Rejecting the request if the passwords are not equal.
+            if (!passwordCompare) return Promise.reject("Invalid password.");
+        })
 ];
 
 module.exports = {
