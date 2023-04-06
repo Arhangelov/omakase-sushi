02a773ccef9c965636469c2d7562580b177282cd
Arhangelov <arhangelov93@gmail.com>
2023-04-06 03:32:46 +0300

    added route for sushi menu controller

--- server/routes.js
+++ server/routes.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userAuthController = require("./controllers/user-auth.controller");
 const createSushiController = require("./controllers/sushi-create.controller");
+const sushiMenuController = require("./controllers/sushi-menu.controller");
 
 router.use("/auth", userAuthController);
 router.use("/createSushi", createSushiController);
+router.use("/menu", sushiMenuController);
 
 module.exports = router;
