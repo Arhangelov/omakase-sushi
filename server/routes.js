9c46ded7f75ffe11dc455781ee4a1853cbcf1b9d
Arhangelov <arhangelov93@gmail.com>
2023-04-09 02:19:39 +0300

    added route for cart controller

--- server/routes.js
+++ server/routes.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const userAuthController = require("./controllers/user-auth.controller");
 const createSushiController = require("./controllers/sushi-create.controller");
 const sushiMenuController = require("./controllers/sushi-menu.controller");
+const cartController = require("./controllers/sushi-cart.controller");
 
 router.use("/auth", userAuthController);
 router.use("/createSushi", createSushiController);
 router.use("/menu", sushiMenuController);
+router.use("/cart", cartController);
 
 module.exports = router;
