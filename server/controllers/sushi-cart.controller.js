5b700db5d77774ca659e5c99a07e0be2a16ec66a
Arhangelov <arhangelov93@gmail.com>
2023-04-09 05:15:25 +0300

    created add to cart request

---
+++ server/controllers/sushi-cart.controller.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { addToCart } = require("../services/sushi-cart.service");
+
+router.post("/addtocart", (req, res) => {
+    addToCart(req.body)
+        .then(cart => {
+            res.json(cart);
+        }).catch(err => {
+            return res.status(400).send({
+                message: `${err.message}`,
+                type: "ERROR"
+            });
+        })
+})
+
+module.exports = router
