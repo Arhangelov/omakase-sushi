87016ceab2a2906efda10a2fc36c790c00fe1179
Arhangelov <arhangelov93@gmail.com>
2023-04-20 00:52:29 +0300

    added home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+//Pages for routing
+import Home from './components/Home/Home'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello from Omakase App</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Hello from Omakase App</h1>
+            <Routes>
+                <Route path='/' element={ <Home /> }/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
