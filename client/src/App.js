7502a7f6a6aaa5293fa921150adc0c16d88c2f66
Arhangelov <arhangelov93@gmail.com>
2023-04-23 16:35:10 +0300

    implemented logout logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,34 @@
 import { useContext } from 'react';
 import './App.css';
-import { Link, Route, Routes } from "react-router-dom";
+import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import { Context } from "./store/UserContext";
+import { logoutService } from './services/userAuth.service';
+import { toast } from 'react-hot-toast';
+import { toastErrorHandler } from './utils/toastErrorHandling';
 //Pages for routing
 import Home from './components/Home/Home';
 import Register from './components/Register/Register';
 
 function App() {
     const [user, setUser] = useContext(Context);
+    const navigate = useNavigate();
+
+    const onLogoutHandler = (e) => {
+        e.preventDefault();
+        logoutService()
+        .then(res => {
+            localStorage.clear();
+            toast.success(`${res.message}`);
+            navigate("/");
+        }).catch(err => {
+            toastErrorHandler(err);
+        })
+    }
     return (
         <div className="App">
             <nav className='container-nav'>
                 {user.username ? (
-                    <Link className='nav-logout'>Logout</Link>
+                    <Link onClick={onLogoutHandler} className='nav-logout'>Logout</Link>
                 ) : (
                     <>
                         <Link className='nav-logo' to='/'>Logo</Link>
