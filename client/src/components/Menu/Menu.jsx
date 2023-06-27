4a265f4bb3e837474fc711033aa1f2cee71936cf
Arhangelov <arhangelov93@gmail.com>
2023-06-28 02:31:17 +0300

    added div for type of sushi links

--- client/src/components/Menu/Menu.jsx
+++ client/src/components/Menu/Menu.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Menu.css";
 
 const Menu = () => {
+
     return (
         <>
-            <h1>Menu Page</h1>
+            <h2>Menu Page</h2>
+            <section className='container-menu'>
+                <Link to="/menu/set" >Set</Link>
+                <Link to="/menu/futomaki" >Futomaki</Link>
+                <Link to="/menu/uramaki" >Uramaki</Link>
+                <Link to="/menu/sashimi" >Sashimi</Link>
+                <Link to="/menu/hosomaki" >Hosomaki</Link>
+                <Link to="/menu/nigiri" >Nigiri</Link>
+            </section>
         </>
     )
 }
