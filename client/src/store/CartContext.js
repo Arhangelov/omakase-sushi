fabaac5612c0811da07f4bfc30c03919ce000d57
Arhangelov <arhangelov93@gmail.com>
2023-07-05 19:50:08 +0300

    updated cart context

--- client/src/store/CartContext.js
+++ client/src/store/CartContext.js
@@ -1,19 +1,41 @@
 import { useReducer, createContext, useContext } from "react";
+import { DECREMENT_CART_ITEM, INCREMENT_CART_ITEM } from "../actions/actionTypes";
 
 export const CartContext = createContext();
+
+const initialState = [
+    {
+        id:"",
+        title:"",
+        img:"",
+        price:"",
+        qty: 0
+    }
+]
 
 const reducer = (state, action) => {
     switch(action.type) {
         case "ADD":
-            return [...state, action.payload];
+            return [
+                ...state,
+                {
+                    id: action.payload.id,
+                    title: action.payload.title,
+                    img: action.payload.img,
+                    price: action.payload.price,
+                    qty: action.payload.qty
+                }
+            ];
         case "REMOVE":
             return state.filter((product) => product.id !== action.payload.id);
     //TODO: Need to handle INCREMENT and DECREMENT logic.
-        case "INCREMENT":
-            // return state.find(product => product.id === action.payload.id);
-            return [ ...state, action.payload.qty + 1 ];
-
-        case "DECREMENT":
+        case INCREMENT_CART_ITEM:
+            const index = state.findIndex(
+                (p) => p.id === action.payload.id
+            );
+            if(index >= 0) state[index].qty += 1
+            return [...state]
+        case DECREMENT_CART_ITEM:
             return state
         default: 
             return state
@@ -21,7 +43,7 @@ const reducer = (state, action) => {
 }
 
 export const CartProvider = ({ children }) => {
-    const [ state, dispatch ] = useReducer( reducer, [] )
+    const [ state, dispatch ] = useReducer( reducer, initialState )
 
     return (
         <CartContext.Provider value={[state, dispatch]}>
