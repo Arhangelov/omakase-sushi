b48fa9a3336bb5285c59275490bf0b12b011ce27
Arhangelov <arhangelov93@gmail.com>
2023-07-14 03:30:38 +0300

    fixed add to cart service and added delete service

--- client/src/services/cart.service.js
+++ client/src/services/cart.service.js
@@ -2,10 +2,18 @@
 const url = "http://localhost:5000/cart";
 
 
-export const addToCartService = async ( sushiProduct, userEmail ) => {
-    return fetch( url + `/addtocart`, {
+export const updateCartService = async ( sushiProduct, userEmail ) => {
+    return fetch( url + `/update-cart`, {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({ sushiProduct, userEmail })
+    }).then(res => res.json());
+};
+
+export const deleteFromCartService = async ( sushiId, userEmail ) => {
+    return fetch( url + `/delete-from-cart`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({ sushiId, userEmail })
     }).then(res => res.json());
 }
