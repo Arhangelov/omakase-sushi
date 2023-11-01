18a068dcd102588a4e7ae89136bf8ae1c1899183
Arhangelov <arhangelov93@gmail.com>
2023-11-01 20:06:27 +0200

    added get details product service

--- client/src/services/menuService.js
+++ client/src/services/menuService.js
@@ -7,3 +7,10 @@ export const getSushiType = async ( sushiType ) => {
         headers: {"Content-Type": "application/json"},
     }).then(res => res.json());
 }
+
+export const getProductDetails = async ( productId ) => {
+    return fetch( url + `/details/${productId}`, {
+        method: "GET",
+        headers: {"Content-Type": "application/json"},
+    }).then(res => res.json());
+}
