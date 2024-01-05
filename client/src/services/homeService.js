df346a9f1d54f4fabde0599da4033d0a307c4510
Arhangelov <arhangelov93@gmail.com>
2024-01-05 21:48:48 +0200

    added popular service request

---
+++ client/src/services/homeService.js
@@ -0,0 +1,11 @@
+// const url = "https://omakase-sushi-api.vercel.app/";
+const url = "http://localhost:5000/";
+
+export const getPopularSushi = async ({ ...popularProducts }) => {
+    console.log(popularProducts);
+    return fetch( url, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify( popularProducts ),
+    }).then(res => res.json());
+}
