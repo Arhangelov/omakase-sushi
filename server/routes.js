afc8ca9bd6d4ace3ea187df2e277a0a3c912ecb9
Arhangelov <arhangelov93@gmail.com>
2024-07-18 23:38:07 +0300

    renamed one route

--- server/routes.js
+++ server/routes.js
@@ -4,14 +4,14 @@ const homeController = require("./controllers/homeController");
 const userAuthController = require("./controllers/userAuthController");
 const createSushiController = require("./controllers/sushiCreateController");
 const sushiMenuController = require("./controllers/sushiMenuController");
-const cartController = require("./controllers/sushiCartController");
+const sushiCartController = require("./controllers/sushiCartController");
 const profileController = require("./controllers/userProfileController");
 
 router.use("/", homeController);
 router.use("/auth", userAuthController);
 router.use("/createSushi", createSushiController);
 router.use("/menu", sushiMenuController);
-router.use("/cart", cartController);
+router.use("/cart", sushiCartController);
 router.use("/profile", profileController);
 
 module.exports = router;
