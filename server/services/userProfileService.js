9d64ea3cd96d498d87f75b1f1a535b93e0b8c711
Arhangelov <arhangelov93@gmail.com>
2024-07-18 23:36:12 +0300

    added calculating functions for totalPrice and sumQty in userService

--- server/services/userProfileService.js
+++ server/services/userProfileService.js
@@ -7,11 +7,25 @@ const getHistoryOfOrders = async ({ userEmail }) => {
 
 const rePurchaseOrder = async ({ cart, totalPrice, userEmail }) => {
     const user = await User.findOne({ email: userEmail });
+    
     user.cart.products = [];
     user.cart.products.push(...cart);
-    user.cart.totalPrice = totalPrice;
 
-    return user.save();
+    //Creating variable for the sum of all quantities of the products and the assign it to the cart.sumQty
+    let sumQty = user.cart.products.reduce((totalQty, cartItem) => {
+        return totalQty + cartItem.qty
+    }, 0);
+    
+    user.cart.sumQty = sumQty;
+
+    let totalPriceActual = user.cart.products.reduce((totalPrice, cartItem) => {
+        return totalPrice + cartItem.price
+    }, 0);
+
+    user.cart.totalPrice = totalPriceActual;
+
+    user.save();
+    return user.cart;
 }
 
 module.exports = {
